bf592ce19f4f0d6217f851cf613a775bda287827
Kamichimi <92457747+Kamichimi@users.noreply.github.com>
2023-01-04 15:02:24 +0100

    Add files via upload
    test pour verifier l'affichage

---
+++ view_begin.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name = "author" content="JoePorthos">
+    <meta name="description" content="Stand de confiseries du BDE">
+    <meta name="keywords" content="bde, stand, confiseries, snack, boissons, iut, villetaneuse, iutv, université, paris, paris13, sorbonne, paris nord">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Stand de confiseries du BDE</title>
+    <link rel="stylesheet" href="Content/css/begin.css">
+</head>
+<body>
+
+     <!-- Entête de la page d'accueil-->
+     <header class="accueil_header">
+        <nav class="accueil_nav">
+            <a href="?controller=home&action=home"><img class="logo_bde" src="Content/img/logo_bde.png" alt="logo-bde" width="70" height="70"></a>
+            <ul>
+                <li><a href="?controller=list&action=confiseries" title="Confisseries">Confisseries</a></li>
+                <li><a href="?controller=list&action=boissons" title="Boissons">Boissons</a></li>
+                <li><a href="page_espace_client.html" target="_blank" title="Mon Espace">Mon Espace</a></li>
+                <li><a href="page_connexion.html" title="Se connecter / S'inscrire">Se connecter / S'inscrire</a></li>
+
+                <!--Menu deroulant pour changer la langue du site-->
+                <li class="li_changer_langue">
+                    <div class="menu_deroulant_langues">
+                        <button class="changer_langues"><img src="Content/img/drapeau_fr.png" alt="logo_français_langue" width="33px" height="23px"> <img src="Content/img/polygone.png" alt="logo_polygone_langue" width="10" height="10"></button>
+                       <!-- <div class="contenu_menu_langues">
+                            <li><a href="#"><img src="../images/drapeau_fr.png" width="43" height="23"></a></li>
+                            <li><a href="#"><img src="../images/logo_english_language.png" width="43" height="23"></a></li>
+                        </div> -->
+                    </div>
+                </li>
+            </ul>
+        </nav>
+    </header>
+    <!--Fin de l'entête de la page-->
+
+    <!--Corps de la page-->
+    <main>
