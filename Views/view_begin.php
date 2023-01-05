1d58535fdee1d8444dfdaf2fb2044a336e8e0996
joe <brandon.bots777@gmail.com>
2023-01-05 16:18:20 +0100

    langue en-fr

--- Views/view_begin.php
+++ Views/view_begin.php
@@ -27,12 +27,10 @@
                 <div class="js">
                     <div class="language-picker js-language-picker" data-trigger-class="btn btn--subtle">
                     <form action="" class="language-picker__form">
-                        <label for="language-picker-select">Select your language</label>
+                        <label for="language-picker-select">Selectionnez votre langue</label>
                         <select name="language-picker-select" id="language-picker-select">
-                        <option lang="de" value="deutsch">Deutsch</option>
-                        <option lang="en" value="english" selected>English</option>
                         <option lang="fr" value="francais">Français</option>
-                        <option lang="it" value="italiano">Italiano</option>
+                        <option lang="en" value="english" selected>English</option>
                         </select>
                     </form>
                     </div></div>
