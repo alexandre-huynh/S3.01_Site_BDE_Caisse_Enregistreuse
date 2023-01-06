9eb01ef3c59d432b2a64258d58a3c52cbcc26a7c
HUYNH Alexandre <12102253@crit-xubuntu>
2023-01-06 15:09:50 +0100

    Modification pour travail en salle machine

--- Utils/credentials.php
+++ Utils/credentials.php
@@ -1,9 +1,16 @@
 <?php
-
+/*
 $dsn="mysql:host=localhost;dbname=e12102253";
 
 $login="e12102253";
 
 $mdp="";
+*/
+
+$dsn="pgsql:host=aquabdd;dbname=etudiants";
+
+$login="12102253";
+
+$mdp="***REMOVED***";
 
  ?>
