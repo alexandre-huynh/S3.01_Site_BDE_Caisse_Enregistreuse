60b7360d4cd17f4e9663cf25b94cb878dd3ce87c
Alexandre Huynh <99390277+alexandre-huynh@users.noreply.github.com>
2023-01-10 16:42:22 +0100

    Modif

--- Views/view_begin.php
+++ Views/view_begin.php
@@ -10,6 +10,8 @@
     <title>Stand de confiseries du BDE</title>
     <link rel="stylesheet" href="Content/css/principal.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <!-- Script JS pour sélection langue déplacé dans Utils/langue.js-->
+    <?php echo '<script src="Utils/langue.js"></script>'; ?>
 </head>
 <body>
 
@@ -45,5 +47,3 @@
     <!--Corps de la page-->
     <main>
 
-    <!-- Script JS pour sélection langue déplacé dans Utils/langue.js-->
-    <?php echo '<script src="Utils/langue.js"></script>'; ?>
