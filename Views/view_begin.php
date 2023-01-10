ba9b835f353d2ecb785244eb18830272abcc402d
Alexandre Huynh <99390277+alexandre-huynh@users.noreply.github.com>
2023-01-10 17:05:53 +0100

    Déplacement script JS

--- Views/view_begin.php
+++ Views/view_begin.php
@@ -12,6 +12,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <!-- Script JS pour sélection langue déplacé dans Utils/langue.js-->
     <!--< ?php echo '<script src="Utils/langue.js"></script>'; ?>-->
+    <?php echo '<script type="text/javascript" src="Utils/caisse_enregist.js"></script>'; ?>
 </head>
 <body>
 
