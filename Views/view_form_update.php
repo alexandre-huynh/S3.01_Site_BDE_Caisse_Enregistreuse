35ab0cf6949f4a9a0ccec2cd682a3f575cba723b
Alexandre Huynh <99390277+alexandre-huynh@users.noreply.github.com>
2023-01-11 15:58:37 +0100

    Début fonction modification produit

--- Views/view_form_update.php
+++ Views/view_form_update.php
@@ -4,46 +4,93 @@
   <?php require "view_begin.php";?>
 <?php endif ?>
 
-<h1> Update a Nobel Prize </h1>
+<!-- Titre de la liste d'élements -->
+<h1> <?= e($titre) ?> </h1>
 
-<form action = "?controller=set&action=update" method="post">
-
+<!--<form action = "?controller=set&action=add_< ?=e($element_to_add)?>" method="post">-->
+<!-- TODO: vérifier dans le cas où le lien redirigé ne marche pas / ou donne la page d'accueil-->
+<form action = "?controller=set&action=update_produit" method="post" enctype="multipart/form-data">  
+  <!--
+  Champs de saisie
+  manuellement rédigé pour adapter le type de saisie
+  -->
+  <!----------------------------- 
+            Ajout produit
+  ------------------------------->
     <p>
-      <input type="hidden" name="id" value="<?=e($id)?>" />
-      <label> Name: <input type="text" name="name" value="<?=e($name)?>"/> </label>
-    </p>
-
-    <p>
-      <label> Year: <input type="text" name="year" value="<?= e($year)?>"/> </label>
-    </p>
-
-    <p>
-      <label> Birth Date: <input type="text" name="birthdate" value="<?= e($birthdate)?>"/></label>
-    </p>
-
-    <p>
-      <label> Birth Place: <input type="text" name="birthplace" value="<?= e($birthplace)?>"/> </label>
+      <label>Identifiant produit* :
+        <input type="number" name="id_produit" value="<?=e($infos["id_produit"])?>" step="1" min="0" readonly  /> 
+      </label>
     </p>
     <p>
-      <label> County: <input type="text" name="county" value="<?= e($county)?>"/> </label>
+      <label>Nom de produit* :
+        <input type="text" name="Nom" value="<?=e($infos["Nom"])?>" maxlength="50"  /> 
+      </label>
     </p>
-
     <p>
-    <?php foreach ($categories as $v): ?>
-      <!-->Faire attention à ne pas mettre d'espace dans value, à part le php <!-->
-        <label> <input type="radio" name="category" value="<?=e($v)?>"
-        <?php
-        if ($v == $category) {
-          echo 'checked="checked"';
-        }
-        ?>
-        /> <?= e(ucfirst($v)) ?>  </label>
-    <?php endforeach ?>
-
+      <label>Catégorie* :
+        <select name="Categorie"  >
+          <!-- TODO: réfléchir si optgroup (grande catégorie) et option de noms similaire = confus? -->
+          <optgroup label="-Snacks-">
+            <option value="Snack" <?php if (e($infos["Categorie"])=="Snack") : ?>selected<?php endif ?>>Snack</option>
+          </optgroup>
+          <optgroup label="-Boissons-">
+            <option value="Boisson" <?php if (e($infos["Categorie"])=="Boisson") : ?>selected<?php endif ?>>Boisson</option>
+            <option value="Soda" <?php if (e($infos["Categorie"])=="Soda") : ?>selected<?php endif ?>>Soda</option>
+            <option value="Sirop" <?php if (e($infos["Categorie"])=="Sirop") : ?>selected<?php endif ?>>Eau + Sirop</option>
+          </optgroup>
+        </select>
+      </label>
     </p>
+    <p>
+      <label>Prix* :
+        <input type="number" name="Prix" value="<?=e($infos["Prix"])?>" step="0.01" min="0" /> €
+      </label>
+    </p>
+    <p>
+      <label>Image du produit* (format .png / .jpg / .jpeg, taille de fichier max : 5 Mo) :
+        <input type="file" name="produit_<?=e($infos["id_produit"])?>" accept=".png,.jpeg,.jpg"  /> 
+        <input type="hidden" name="Img_produit" value="produit_<?=e($infos["id_produit"])?>" />
+      </label>
+      </br>
+      <span>Fond transparent de préférence : Google Images > Outils > Couleur > Transparent</span>
+    </p>
+    <p>
+      <label>Date de création* (corriger si nécessaire) :
+        <input type="date" name="Date_ajout" value="<?=e($infos["Date_creation"])?>"  /> 
+      </label>
+    </p>
+    <p>
+      <label>Nombre de points fidélité requis pour obtenir ce produit gratuitement* :
+        <input type="number" name="Pts_fidelite_requis" value="<?=e($infos["Pts_fidelite_requis"])?>" step="1" min="0" /> pts
+      </label>
+    </p>
+    <p>
+      <label>Nombre de points fidélité octroyés/donnés lors de l'achat de ce produit* :
+        <input type="number" name="Pts_fidelite_donner" value="<?=e($infos["Pts_fidelite_donner"])?>" step="1" min="0" /> pts
+      </label>
+    </p>
+    <p>
+      <label>Stock disponible* :
+        <input type="number" name="Stock" value="<?=e($infos["Stock"])?>" step="1" min="0"  />
+      </label>
+    </p>
+    <p>
+      <label>Ventes effectués* :
+        <input type="number" name="Nb_ventes" value="<?=e($infos["Nb_ventess"])?>" step="1" min="0"  />
+      </label>
+    </p>
+  <!--
+  Champs de saisie par défaut (toutes les saisies sont des champs textes)
+  au cas où s'il peut être utile
+  non testé
+  < ?php foreach ($attributs as $valeur): ?>
+    <p><label> < ?=e($valeur)?> <input type="text" name="< ?=e($valeur)?>" /> </label></p>
+  < ?php endforeach ?>
+  -->
 
-    <textarea name="motivation" cols="70" rows="10"> <?= e($motivation) ?> </textarea>
-    <p>  <input type="submit" value="Update the database"/> </p>
+  <!--Validation-->
+  <p> <input type="submit" value="Modifier le produit"/> </p>
 </form>
 
 <?php require "view_end.php";?>
