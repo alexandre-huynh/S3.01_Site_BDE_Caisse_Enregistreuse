b98d5f05f1d3f4b74a43d8b97e7956f6ae950789
Gupta <guptagivarun@gmail.com>
2023-01-11 16:52:50 +0100

    message

--- Utils/caisse_enregist.js
+++ Utils/caisse_enregist.js
@@ -206,7 +206,7 @@ function clickSnacks(){
     ligne.textContent=event.target.textContent;
     addSnacks(ligne);
     let panier = document.querySelectorAll('panier li');
-    panier.append(ligne);
+    panier.push(ligne);
             for(var value of panier.values()){
             if (ligne.textContent==value){
                 panier.remove(ligne);
