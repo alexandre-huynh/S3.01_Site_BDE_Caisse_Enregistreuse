647b966fd3e8e541577aadf9650c91ae15bd2c33
Alexandre Huynh <alexandre8691@gmail.com>
2023-01-11 22:17:07 +0100

    Modif CSS rapide

--- Views/view_begin_connected.php
+++ Views/view_begin_connected.php
@@ -23,7 +23,7 @@
                 <a href="?controller=list&action=<?php if (isset($_SESSION["statut"]) && $_SESSION["statut"]=="admin") : ?>espace_admin<?php elseif (isset($_SESSION["statut"]) && $_SESSION["statut"]=="client") : ?>espace_client<?php endif ?>" title="Mon Espace">Mon Espace</a>
                 <a href="?controller=list&action=infos_compte" title="Mes infos">Mes Infos</a>
                 <img src="Content/img/logo_user.png" width="40px" alt="Image Logo Utilisateur" />  <?=e($_SESSION["prenomnom"])?> (<?=e($_SESSION["num_etudiant"])?>)
-                <a href="?controller=auth&action=logout" title="Se déconnecter">Se déconnecter</a>
+                <a href="?controller=auth&action=logout" title="Se déconnecter" class="deco">Se déconnecter</a>
             </div>
         </nav>
 
