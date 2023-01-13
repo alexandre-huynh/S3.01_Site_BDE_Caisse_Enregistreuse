e0f2a6609b9eb0521d7233567009abaad9b922c4
Cleante Aubert <cleante95@gmail.com>
2023-01-13 16:10:19 +0100

    controler_set fonction action_remove_produit

--- Controllers/Controller_set.php
+++ Controllers/Controller_set.php
@@ -548,7 +548,7 @@ class Controller_set extends Controller{
   // !!! DISTINGUER REMOVE PRODUIT, REMOVE CLIENT ETC? !!!
   // modifier les liens dans les view de modification pour que ça utilise bien l'action en question
 
-  public function action_remove() {
+  public function action_remove_produit() {
     $m = Model::getModel();
     //==================================
     //     TEST SI C'EST UN ADMIN
@@ -556,11 +556,24 @@ class Controller_set extends Controller{
     if (!isset($_SESSION['connected']) || !isset($_SESSION['statut']) || !isset($_SESSION['id_admin']) || !$_SESSION['connected'] || $_SESSION['statut']!='admin' || !$m->isInDatabaseAdmin($_SESSION["email"])){
       $this->action_error("Vous ne possédez pas les droits administrateurs pour consulter cette page.");
     }
-    //===================================
+    if(!isset($_GET['id'])){
+      $this->action_error("Erreur l'identifiant du produit n'a pas été renseigné");
+    }
+    else{
+    
+      // Afficher message, êtes vous sur de vouloir supprimer le produit ? 
+      // Ceci entrainera la suppression des ventes associés à ce produit 
+  
+    // Associer à la fonction removeProduit de model.php pour supprimer le produit 
+    $m->removeProduit($_GET['id']);
+    }
+
 
     $data = [
-      "title" => "Removing a Nobel Prize.",
-      "message" => $message
+      "title" => "Supprimer un produit",
+      "message" => "Le produit à été supprimé avec succès",
+      "str_lien_retour" => "Retour à la page de l'inventaire",
+      "lien_retour" => "?controller=list&action=gestion_inventaire" 
     ];
     $this->render("message", $data);
   }
@@ -589,213 +602,7 @@ class Controller_set extends Controller{
       ]; 
 
     $this->render("form_update_produit", $data);
-  
-  }
-
-  public function action_update_produit(){
-    $m = Model::getModel();
-    //==================================
-    //     TEST SI C'EST UN ADMIN
-    //==================================
-    if (!isset($_SESSION['connected']) || !isset($_SESSION['statut']) || !isset($_SESSION['id_admin']) || !$_SESSION['connected'] || $_SESSION['statut']!='admin' || !$m->isInDatabaseAdmin($_SESSION["email"])){
-      $this->action_error("Vous ne possédez pas les droits administrateurs pour consulter cette page.");
     }
-    //===================================
-
-    $ajout = false;
-
-    if (isset($_POST["id_produit"]) &&
-      isset($_POST["Nom"]) &&
-      isset($_POST["Categorie"]) &&
-      isset($_POST["Prix"]) &&
-      isset($_POST["Date_ajout"]) &&
-      isset($_POST["Pts_fidelite_requis"]) &&
-      isset($_POST["Pts_fidelite_donner"]) &&
-      isset($_POST["Stock"]) &&
-      isset($_POST["Nb_ventes"])) 
-    {
-      $produit = $m->getProduitPrecis($_POST["id_produit"]);
-
-      // Préparation du tableau infos
-      foreach($produit as $c=>$v){
-        if ($c!="Img_produit" && $v!=$_POST[$c]){
-          $ajout = $m->updateProduit($_POST["id_produit"], $c, $_POST[$c]);
-        }
-      }
-
-      //=============Changement de l'image====================
-      if (isset($_POST["Update_img"]) && $_POST["Update_img"]){
-
-        $msg_error = "";
-
-        $target_dir = "Content/img/";
-        $target_file = $target_dir . $produit["Img_produit"];//basename($_FILES[$_POST["Img_produit"]]["name"]);
-        $uploadOk = 1;
-        $temporaire= $target_dir . basename($_FILES[$produit["Img_produit"]]["name"]);
-        $imageFileType = strtolower(pathinfo($temporaire,PATHINFO_EXTENSION));
-        //$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-        // Check if image file is a actual image or fake image
-        if(isset($_POST["submit"])) {
-          $check = getimagesize($_FILES[$produit["Img_produit"]]["tmp_name"]);
-          if($check !== false) {
-            //echo "File is an image - " . $check["mime"] . ".";
-            $uploadOk = 1;
-          } else {
-            $msg_error = $msg_error . "File is not an image.";
-            $uploadOk = 0;
-          }
-        }
-
-        // Allow certain file formats
-        if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" ) {
-          $msg_error = $msg_error . "Sorry, only JPG, JPEG, PNG files are allowed.";
-          $uploadOk = 0;
-        }
-        
-        // Suppression / délien de l'ancienne image
-        if(file_exists($target_file)) {
-          unlink($target_file . $imageFileType); //remove the file
-        }
-
-        // Check if $uploadOk is set to 0 by an error
-        if ($uploadOk == 0) {
-          $this->action_error("Sorry, your file was not uploaded : " . $msg_error);
-        // if everything is ok, try to upload file
-        } else {
-          if (move_uploaded_file($_FILES[$produit["Img_produit"]]["tmp_name"], $target_file . "." . $imageFileType)) {
-            //echo "The file ". e( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
-            $ajout = ok;
-          } else {
-            $this->action_error("Sorry, there was an error uploading your file :" . $msg_error);
-          }
-        }
-      }
-      //===============fin changement image=================
-    }
-
-    //Préparation de $data pour l'affichage de la vue message
-    $data = [
-        "title" => "Modification du produit",
-        "str_lien_retour" => "Retour à la page de l'inventaire des produits",
-        "lien_retour" => "?controller=list&action=gestion_inventaire" 
-    ];
-    if ($ajout) {
-        $data["message"] = "Le produit " . $_POST["Nom"] . " a été mis à jour avec succès.";
-    } else {
-        $data["message"] = "Erreur dans la saisie des informations, le produit n'a pas été modifié.";
-    }
-
-    $this->render("message", $data);
-  }
-  
-  public function action_update_client(){
-    $m = Model::getModel();
-    //==================================
-    //     TEST SI C'EST UN ADMIN
-    //==================================
-    if (!isset($_SESSION['connected']) || !isset($_SESSION['statut']) || !isset($_SESSION['id_admin']) || !$_SESSION['connected'] || $_SESSION['statut']!='admin' || !$m->isInDatabaseAdmin($_SESSION["email"])){
-      $this->action_error("Vous ne possédez pas les droits administrateurs pour consulter cette page.");
-    }
-    //===================================
-
-    $ajout = false;
-
-    if (isset($_POST["id_client"]) && 
-      isset($_POST["num_etudiant"]) && 
-      isset($_POST["Nom"]) && 
-      isset($_POST["Prenom"]) && 
-      isset($_POST["Email"]) &&
-      isset($_POST["Date_creation"]) && 
-      isset($_POST["Pts_fidelite"])) 
-    {
-      $client = $m->getClientPrecis($_POST["id_client"]);
-
-      // Préparation du tableau infos
-      foreach($client as $c=>$v){
-        if ($v!=$_POST[$c]){
-          $ajout = $m->updateClient($_POST["id_client"], $c, $_POST[$c]);
-        }
-      }
-
-      if (isset($_POST["Password"]) && 
-        isset($_POST["Password_verify"]) && 
-        ! preg_match("/^ *$/", $_POST["Password"]))
-      {
-        if ($_POST["Password"]!=$_POST["Password_verify"]) {
-          $this->action_error("Le mot de passe saisi ne correspond pas au mot de passe de confirmation. Néanmoins, si des informations client ont été modifiés précédemment, ces changements ont bien été pris en compte.");
-        }
-        $m->updatePassword($_POST["Email"],password_hash($_POST["Password"], PASSWORD_DEFAULT),"Client");     
-      }
-    }
-
-    //Préparation de $data pour l'affichage de la vue message
-    $data = [
-        "title" => "Modification des infos client",
-        "str_lien_retour" => "Retour à la page de gestion des comptes clients",
-        "lien_retour" => "?controller=list&action=gestion_clients" 
-    ];
-    if ($ajout) {
-        $data["message"] = "Les informations du client  " . $_POST["Prenom"] . $_POST["Nom"] . " ont été mis à jour avec succès.";
-    } else {
-        $data["message"] = "Erreur dans la saisie des informations, les informations du client n'ont pas été modifié.";
-    }
-
-    $this->render("message", $data);
-  }
-
-  public function action_update_admin(){
-    $m = Model::getModel();
-    //==================================
-    //  TEST SI C'EST UN SUPER ADMIN
-    //==================================
-    if (!isset($_SESSION['connected']) || !isset($_SESSION['statut']) || !isset($_SESSION['id_admin']) || !$_SESSION['connected'] || $_SESSION['statut']!='admin' || !$m->isInDatabaseAdmin($_SESSION["email"]) || !$m->isInDatabaseSuperAdmin($_SESSION["id_admin"])){
-      $this->action_error("Vous ne possédez pas les droits super administrateurs pour consulter cette page.");
-    }
-    //===================================
-
-    $ajout = false;
-
-    if (isset($_POST["id_admin"]) && 
-      isset($_POST["num_etudiant"]) && 
-      isset($_POST["Nom"]) && 
-      isset($_POST["Prenom"]) && 
-      isset($_POST["Email"]) &&
-      isset($_POST["Date_creation"]) && 
-      isset($_POST["Pts_fidelite"])) 
-    {
-      $admin = $m->getAdminPrecis($_POST["id_admin"]);
-
-      // Préparation du tableau infos
-      foreach($admin as $c=>$v){
-        if ($v!=$_POST[$c]){
-          $ajout = $m->updateAdmin($_POST["id_admin"], $c, $_POST[$c]);
-        }
-      }
-
-      if (isset($_POST["Password"]) && 
-        isset($_POST["Password_verify"]) && 
-        ! preg_match("/^ *$/", $_POST["Password"]))
-      {
-        if ($_POST["Password"]!=$_POST["Password_verify"]) {
-          $this->action_error("Le mot de passe saisi ne correspond pas au mot de passe de confirmation. Néanmoins, si des informations admin ont été modifiés précédemment, ces changements ont bien été pris en compte.");
-        }
-        $m->updatePassword($_POST["Email"],password_hash($_POST["Password"], PASSWORD_DEFAULT),"Admin");     
-      }
-    }
-
-    //Préparation de $data pour l'affichage de la vue message
-    $data = [
-        "title" => "Modification des infos admin",
-        "str_lien_retour" => "Retour à la page de gestion des comptes admins",
-        "lien_retour" => "?controller=list&action=gestion_admins" 
-    ];
-    if ($ajout) {
-        $data["message"] = "Les informations de l'admin  " . $_POST["Prenom"] . $_POST["Nom"] . " ont été mis à jour avec succès.";
-    } else {
-        $data["message"] = "Erreur dans la saisie des informations, les informations du admin n'ont pas été modifié.";
-    }
-
-    $this->render("message", $data);
   }
 
   public function action_update(){
@@ -809,7 +616,37 @@ class Controller_set extends Controller{
     }
     //===================================
 
-    
+    $in_database = false;
+
+    if (isset($_POST["id"]) and preg_match("/^[1-9]\d*$/",$_POST["id"])
+      and isset($_POST["name"]) and ! preg_match ("/^ *$/", $_POST["name"])
+      and isset($_POST["category"]) and ! preg_match("/^ *$/", $_POST["category"])
+      and isset($_POST["year"]) and preg_match("/^[12]\d{3}$/", $_POST["year"])) {
+      $m = Model::getModel();
+      $c_in_database = in_array($_POST["category"], $m->getCategories());
+      $in_database = $c_in_database ? $m->isInDatabase($_POST["id"]) : false;
+    }
+    if ($in_database) {
+      // Préparation $infos pour la mise à jour des informations du prix nobels
+      $infos = [];
+      $noms =  ["id", "year", "category", "name", "birthdate", "birthplace", "county", "motivation"];
+      foreach ($noms as $v) {
+        if (isset($_POST[$v]) and ! preg_match("/^ *$/", $_POST[$v])) {
+          $infos[$v] = $_POST[$v];
+        } else {
+          $infos[$v] = null;
+        }
+      }
+      $m->updateNobelPrize($infos);
+      $message = "The informations of the Nobel Prize have been updated.";
+    } else {
+      $message = "There is no information to update.";
+    }
+    $data = [
+      "title" => "Updating the Nobel Prize informations",
+      "message" => $message
+    ];
+    $this->render("message", $data);
   }
 
   public function action_update_infos(){
@@ -927,6 +764,8 @@ class Controller_set extends Controller{
   public function action_default(){
     $this->action_form_add();
   }
+
+  
 
 }
 
