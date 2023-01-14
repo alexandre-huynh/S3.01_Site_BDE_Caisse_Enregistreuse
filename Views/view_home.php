62eecd36917c3fa02b2ff86abcb4e95dc4c46f41
joe <brandon.bots777@gmail.com>
2023-01-14 17:39:45 +0100

    Update view_home.php

--- Views/view_home.php
+++ Views/view_home.php
@@ -1,11 +1,9 @@
-<link rel="stylesheet" type="text/css" href="Content/css/home.css">
-<!--< ?php require "view_begin.php"; ?>-->
-
 <?php if (isset($_SESSION["connected"]) && $_SESSION["connected"]==True) : ?>
   <?php require "view_begin_connected.php";?>
 <?php else : ?>
   <?php require "view_begin.php";?>
 <?php endif ?>
+<link rel="stylesheet" type="text/css" href="Content/css/home.css">
 
         <section class="banniere_accueil">
             <div class="phrase_bienvenue">
