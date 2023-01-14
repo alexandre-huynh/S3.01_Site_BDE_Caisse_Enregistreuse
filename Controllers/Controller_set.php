d73d12fc538486ebffa5e3e1183869794fbf5e3c
Alexandre Huynh <alexandre8691@gmail.com>
2023-01-14 16:29:30 +0100

    Update Controller_set.php

--- Controllers/Controller_set.php
+++ Controllers/Controller_set.php
@@ -1026,7 +1026,7 @@ class Controller_set extends Controller{
 
       // Préparation du tableau infos
       foreach($client as $c=>$v){
-        if ($v!=$_POST[$c] && $c!="id_client" && $c!="Pts_fidelite" && $c!="Date_Creation" ){
+        if ($v!=$_POST[$c]){
           // si le num étudiant est modifié, modif dans client puis dans authentif
           if ($c=="num_etudiant"){
             // si existe déjà, pas de modif, erreur
@@ -1085,7 +1085,7 @@ class Controller_set extends Controller{
 
       // Préparation du tableau infos
       foreach($admin as $c=>$v){
-        if ($v!=$_POST[$c] && $c!="id_admin" && $c!="Pts_fidelite" && $c!="Date_Creation" ){
+        if ($v!=$_POST[$c]){
           // si le num étudiant est modifié, modif dans admin puis dans authentif
           if ($c=="num_etudiant"){
             // si existe déjà, pas de modif, erreur
