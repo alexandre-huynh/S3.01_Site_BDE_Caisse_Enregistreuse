3c4903eb40cb1c7252b83ab33e9fb5e5ffdd046d
joe <brandon.bots777@gmail.com>
2023-01-18 14:01:03 +0100

    bg-white userName connected

--- Views/view_begin_connected.php
+++ Views/view_begin_connected.php
@@ -24,7 +24,7 @@
                 <a href="?controller=list&action=boissons" title="Boissons" class="deco">Boissons</a>
                 <a href="?controller=list&action=<?php if (isset($_SESSION["statut"]) && $_SESSION["statut"]=="admin") : ?>espace_admin<?php elseif (isset($_SESSION["statut"]) && $_SESSION["statut"]=="client") : ?>espace_client<?php endif ?>" title="Mon Espace" class="deco">Mon Espace</a>
                 <a href="?controller=list&action=infos_compte" title="Mes infos" class="deco">Mes Infos</a>
-                <img src="Content/img/logo_user.png" width="40px" alt="Image Logo Utilisateur" />  <?=e($_SESSION["prenomnom"])?> (<?=e($_SESSION["num_etudiant"])?>)
+                <img src="Content/img/logo_user.png" width="40px" alt="Image Logo Utilisateur" /> <span class="decore_white"> <?=e($_SESSION["prenomnom"])?> (<?=e($_SESSION["num_etudiant"])?>)</span>
                 <a href="?controller=auth&action=logout" title="Se déconnecter" class="deco">Se déconnecter</a>
             </div>
         </nav>
