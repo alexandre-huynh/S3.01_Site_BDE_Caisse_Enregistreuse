6b748382d6b98932d5dff849ed4eb6aa3fe159a8
Alexandre Huynh <alexandre8691@gmail.com>
2023-01-20 02:09:41 +0100

    Update view_caisse.php

--- Views/view_caisse.php
+++ Views/view_caisse.php
@@ -23,8 +23,8 @@
             -->
         </ul>
         <p class="total">TOTAL : <span id="totalprix">0</span><span class="devise"> €</span></p>
-        <p>Payé : <span id="dejapayer">0</span><span class="devise"> €</span></p>
-        <p>Reste à payer : <span id="reste">0</span><span class="devise"> €</span></p>
+        <p class="dejapayer">Payé : <span id="dejapayer">0</span><span class="devise"> €</span></p>
+        <p class="reste">Reste à payer : <span id="reste">0</span><span class="devise"> €</span></p>
         <p>Paiement par : <b><span id="paiement">Espèce</span></b></p>
     </div>
     <!-- 
